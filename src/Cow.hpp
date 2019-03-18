c10d0cf9881c82a01e441fbea5f9a2e97001d85d
Dandi99 <d.agusmaulana99@gmail.com>
2019-03-18 19:50:55 +0700

    Create Cow.hpp

---
+++ src/Cow.hpp
@@ -0,0 +1,17 @@
+#ifndef COW_HPP
+#define COW_HPP
+
+#include "FarmAnimal.hpp.hpp"
+
+
+class Cow : public FarmAnimal {
+
+    private :
+
+    public :
+        Cow(); //Konstruktor
+        void Sounds(); //Menghasilkan suara cow
+
+};
+
+#endif
