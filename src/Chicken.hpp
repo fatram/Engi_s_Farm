4ecd86c2efc69b8552c23702451e75cc4c704481
Dandi99 <d.agusmaulana99@gmail.com>
2019-03-18 19:52:43 +0700

    Create Chicken.hpp

---
+++ src/Chicken.hpp
@@ -0,0 +1,17 @@
+#ifndef CHICKEN_HPP
+#define CHICKEN_HPP
+
+#include "FarmAnimal.hpp"
+#include "MeatPFA.h"
+
+class Chicken : public FarmAnimal {
+
+    private :
+
+    public :     
+        Chicken();// Konstruktor 
+        void Sounds(); //Menghasilkan suara chicken
+
+};
+
+#endif
