2c201becb45dd3ee387ac9c6a8c6e95dd6fd1e00
fatram <43745905+fatram@users.noreply.github.com>
2019-03-19 00:12:02 +0700

    Update Camel.hpp

--- src/Camel.hpp
+++ src/Camel.hpp
@@ -3,13 +3,14 @@
 
 #include "FarmAnimal.hpp"
 
-class Camel : public FarmAnimal {
+class Camel : public FarmAnimal, Renderable {
 
     private :
 
     public :     
         Camel();// Konstruktor 
         void Sounds(); //Menghasilkan suara camel
+        char render(); //Mengembalikan karakter 'U' sebagai representasi objek Camel
 
 };
 
