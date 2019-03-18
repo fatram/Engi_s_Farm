65911cf2054c281f9c43f4c17e61cd52dcaecaa5
Dandi99 <d.agusmaulana99@gmail.com>
2019-03-18 19:58:02 +0700

    Create Camel.hpp

---
+++ src/Camel.hpp
@@ -0,0 +1,16 @@
+#ifndef CAMEL_HPP
+#define CAMEL_HPP
+
+#include "FarmAnimal.hpp"
+
+class Camel : public FarmAnimal {
+
+    private :
+
+    public :     
+        Camel();// Konstruktor 
+        void Sounds(); //Menghasilkan suara camel
+
+};
+
+#endif
