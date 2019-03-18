8e84288ae334dba469dd948ee3d217b757df6988
Dandi99 <d.agusmaulana99@gmail.com>
2019-03-18 19:54:16 +0700

    Create Goat.hpp

---
+++ src/Goat.hpp
@@ -0,0 +1,16 @@
+#ifndef GOAT_HPP
+#define GOAT_HPP
+
+#include "FarmAnimal.hpp"
+
+class Chicken : public FarmAnimal {
+
+    private :
+
+    public :     
+        Goat();// Konstruktor 
+        void Sounds(); //Menghasilkan suara chicken
+
+};
+
+#endif
