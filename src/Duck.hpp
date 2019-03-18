eea86abed51c718162dfeb8c133fc556c28166f0
Dandi99 <d.agusmaulana99@gmail.com>
2019-03-18 19:57:18 +0700

    Create Duck.hpp

---
+++ src/Duck.hpp
@@ -0,0 +1,16 @@
+#ifndef DUCK_HPP
+#define DUCK_HPP
+
+#include "FarmAnimal.hpp"
+
+class Chicken : public FarmAnimal {
+
+    private :
+
+    public :     
+        Duck();// Konstruktor 
+        void Sounds(); //Menghasilkan suara Duck
+
+};
+
+#endif
