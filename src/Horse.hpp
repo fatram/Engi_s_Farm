cc9a5a809f5ba264ae72d5feead7bad90704ca1b
Dandi99 <d.agusmaulana99@gmail.com>
2019-03-18 19:55:42 +0700

    Create Horse.hpp

---
+++ src/Horse.hpp
@@ -0,0 +1,16 @@
+#ifndef HORSE_HPP
+#define HORSE_HPP
+
+#include "FarmAnimal.hpp"
+
+class Horse : public FarmAnimal {
+
+    private :
+
+    public :     
+        Horse();// Konstruktor 
+        void Sounds(); //Menghasilkan suara horse
+
+};
+
+#endif
