e4945201fde1767d9990d7a07abf4777777e6fdc
fatram <43745905+fatram@users.noreply.github.com>
2019-03-19 00:14:18 +0700

    Update Cow.hpp

--- src/Cow.hpp
+++ src/Cow.hpp
@@ -2,14 +2,16 @@
 #define COW_HPP
 
 #include "FarmAnimal.hpp"
+#include "Renderable.hpp"
 
-class Cow : public FarmAnimal {
+class Cow : public FarmAnimal, Renderable {
 
     private :
 
     public :
         Cow(); //Konstruktor
         void Sounds(); //Menghasilkan suara cow
+        char render(); //Mengembalikan karakter 'S' sebagai representasi objek Cow
 
 };
 
