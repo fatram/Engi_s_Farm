e62abc466f25c2e346497b0402475c6e9037da7a
fatram <43745905+fatram@users.noreply.github.com>
2019-03-28 11:55:55 +0700

    Create Cell.cpp

---
+++ src/Cell.cpp
@@ -0,0 +1,41 @@
+#include "Cell.hpp"
+
+Cell::Cell(){
+  coolDownTruck = 0;
+  hasFacility = false;
+  hasGrass = false;
+}
+
+Cell::Cell(int cd, bool hf, bool hg){
+  coolDownTruck = cd;
+  hasFacility = hf;
+  hasGrass = hg;
+}
+
+void Cell::grow(){
+  setHasGrass(true);
+}
+
+int Cell::getCoolDownTruck() const {
+  return coolDownTruck;
+}
+
+void Cell::setCoolDownTruck(int cd){
+  coolDownTruck = cd;
+}
+
+bool Cell::getHasFacility() const {
+  return hasFacility;
+}
+
+void Cell::setHasFacility(bool hf) {
+  hasFacility = hf;
+}
+
+bool Cell::getHasGrass() const {
+  return hasGrass;
+}
+
+void Cell::setHasGrass(bool hg) {
+  hasGrass = hg;
+}
