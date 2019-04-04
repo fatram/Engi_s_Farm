d41da9f0b2fbb411e3d1f0a843d8d2b489876193
fatram <43745905+fatram@users.noreply.github.com>
2019-04-04 12:26:52 +0700

    Add files via upload

--- src/Mixer.cpp
+++ src/Mixer.cpp
@@ -11,12 +11,81 @@ char Mixer::render() {
 //Player dipersilakan memilih product dari bag minimal 2 buah
 //Hasil mix menjadi side product dimasukkan ke dalam bag 
 void Mixer::interact(Player& P){
-    // Tampilkan daftar product di bag Player
-    // belum diimplementasi
-
-    // Persilakan player memilih product, simpan di p1 & p2
-    Product* p1;
-    Product* p2;
-    if (p1->toString().compare("Duck Egg") == 0)
-    // Menghasi
+	int choice;
+	cout << "Resep yang tersedia : " << endl;
+	cout << "1. Pancake" << endl;
+	cout << "   Ingredients : Chicken's Egg + Cow's Milk" << endl;
+	cout << "2. Omelette" << endl;
+	cout << "   Ingredients : Duck's Egg + Camel's Milk" << endl;
+	cout << "3. Sausage" << endl;
+	cout << "   Ingredients : Goat's Meat + Horse's Meat" << endl;
+	cout << "Pilih item yang Anda inginkan (1/2/3) : " << endl;
+	do{
+		choice >> cin;
+		if(choice != 1 && choice != 2 && choice != 3){
+			cout << "Tolong masukkan angka 1, 2, atau 3" << endl;
+		}
+	}while(choice != 1 && choice != 2 && choice != 3);
+	if(bag.size() > 0){
+		cout << "Item di dalam bag : " << endl;
+		for(int i = 0; i < P.bag.size(); i++){
+			cout << (i+1) + ". " + P.bag.get(i).toString() << endl;
+		}
+		cout << "Pilih item yang akan digunakan (e.g 1 2 (Artinya item nomor 1 dan 2) : "
+		int c1;
+		int c2;
+		cin >> c1;
+		cin >> c2;
+		if(c1 > P.bag.size() || c2 > P.bag.size()){
+			cout << "Item yang dipilih tidak tersedia" << endl;
+		} else {
+			Product * p1 = P.bag.get(c1-1);
+			Product * p2 = P.bag.get(c2-2);
+			if(choice == 1){
+				if((p1->toString().compare("Chicken Egg") == 0
+					&& p2->toString().compare("Cow Milk") == 0) ||
+					(p2->toString().compare("Chicken Egg") == 0
+					&& p1->toString().compare("Cow Milk") == 0)){
+						Pancake pc;
+						Product * ppc = &pc;
+						P.bag.remove(p1);
+						P.bag.remove(p2);
+						P.bag.add(ppc);
+						cout << "Pancake berhasil dibuat" << endl;
+				} else {
+					cout << "Pancake gagal dibuat" << endl;
+				}
+			} else if(choice == 2){
+				if((p1->toString().compare("Duck Egg") == 0
+					&& p2->toString().compare("Camel Milk") == 0) ||
+					(p2->toString().compare("Duck Egg") == 0
+					&& p1->toString().compare("Camel Milk") == 0)){
+						Omelette o;
+						Product * ppc = &o;
+						P.bag.remove(p1);
+						P.bag.remove(p2);
+						P.bag.add(ppc);
+						cout << "Omelette berhasil dibuat" << endl;
+				} else {
+					cout << "Omelette gagal dibuat" << endl;
+				}
+			} else {
+				if((p1->toString().compare("Goat Meat") == 0
+					&& p2->toString().compare("Horse Meat") == 0) ||
+					(p2->toString().compare("Goat Meat") == 0
+					&& p1->toString().compare("Horse Meat") == 0)){
+						Sausage s;
+						Product * ppc = &s;
+						P.bag.remove(p1);
+						P.bag.remove(p2);
+						P.bag.add(ppc);
+						cout << "Sausage berhasil dibuat" << endl;
+				} else {
+					cout << "Sausage gagal dibuat" << endl;
+				}
+			}
+		}
+	} else {
+		cout << "Tas anda kosong" << endl;
+	}
 }
