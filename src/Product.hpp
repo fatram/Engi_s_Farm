5344a21be288db7684c13f02e5af3513226caaae
fatram <43745905+fatram@users.noreply.github.com>
2019-04-04 11:47:05 +0700

    Update Product.hpp

--- src/Product.hpp
+++ src/Product.hpp
@@ -16,9 +16,6 @@ class Product {
 		//user-define ctor
 	 	Product(int h);
 
-		 //pure virtual dtor agar menjadi kelas abstrak
-		virtual ~Product() = 0;
-
 		virtual string toString() = 0; //revisi, mendapatkan nama tiap kelas 
 
 		//setter harga
